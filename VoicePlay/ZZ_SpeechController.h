c1899575b66c37f21034f09fca109252cca6c3a1
heze <www.895315401@qq.com>
2018-04-22 17:23:21 +0800

    第一次提交

---
+++ VoicePlay/ZZ_SpeechController.h
@@ -0,0 +1,13 @@
+//
+//  ZZ_SpeechController.h
+//  VoicePlay
+//
+//  Created by 何泽的mac on 2018/4/22.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZ_SpeechController : NSObject
+
+@end
---
+++ VoicePlay/ZZ_SpeechController.m
@@ -0,0 +1,13 @@
+//
+//  ZZ_SpeechController.m
+//  VoicePlay
+//
+//  Created by 何泽的mac on 2018/4/22.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import "ZZ_SpeechController.h"
+
+@implementation ZZ_SpeechController
+
+@end
